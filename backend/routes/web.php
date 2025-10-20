ce170d5a2422ae6018b55c5cb2ee03fe2935907d
Blazeman430 <148768527+Blazeman430@users.noreply.github.com>
2025-10-21 00:19:22 +0800

    Delete backend directory

--- backend/routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
-
-Route::middleware('web')->group(function () {
-    Route::post('/api/auth/login', [AuthController::class, 'login']);
-});
